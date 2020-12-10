58c9669b0e97f4c627b721d80f1e7985dfc4e147
the22mastermind <bertrandmasabo22@gmail.com>
2020-12-10 02:15:11 +0200

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { AddToDoComponent } from './components/add-to-do/add-to-do.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddToDoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { TodoService } from './todo.service';
 
@@ -6,11 +7,55 @@ describe('TodoService', () => {
   let service: TodoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    TestBed.configureTestingModule({
+      providers: [TodoService]
+    });
+    // service = TestBed.inject(TodoService);
+    service = TestBed.get(TodoService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('Todos', () => {
+    it('should return a list of todos', () => {
+      const TodosResponse = [
+        {
+          id: 1,
+          title: 'delectus aut autem',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'quis ut nam facilis et officia qui',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'fugiat veniam minus',
+          completed: false,
+        },
+        {
+          id: 4,
+          title: 'et porro tempora',
+          completed: true,
+        },
+        {
+          id: 5,
+          title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+          completed: false,
+        },
+      ];
+      let response;
+
+      spyOn(service, 'getTodos').and.returnValue(of(TodosResponse));
+      service.getTodos().subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(TodosResponse);
+    });
+  });
+
 });
